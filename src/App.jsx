9486cb245712beb269206dd4d82c5be350a54af8
Jitendra-civil <jitendrapanigrahi16@gmail.com>
2024-07-22 14:27:33 +0530

    Vidtube repo

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React, { useState } from 'react'
+import Home from './Pages/Home/Home'
+import Video from './Pages/Video/Video'
+import Navbar from './Components/Navbar/Navbar'
+import { Routes, Route } from 'react-router-dom'
+const App = () => {
+
+  const [sidebar,setSidebar] = useState(true);
+
+  return (
+    <div>
+      <Navbar setSidebar={setSidebar}/>
+      <Routes>
+        <Route path='/' element = {<Home sidebar={sidebar}/>}/>
+        <Route path ='/video/:categoryId/:videoId' element={<Video/>}/>
+      </Routes>
+    </div>
+  )
+}
+
+export default App
